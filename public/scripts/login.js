14966eba0615b422cb4a6d51d8720d94dab0eea9
Kijk_aq72@student.itstep.org <Kijk_aq72@student.itstep.org>
2025-06-26 00:37:27 +0300

    Добавлен код

--- public/scripts/login.js
+++ public/scripts/login.js
@@ -7,7 +7,7 @@ document.addEventListener("DOMContentLoaded", function () {
       const password = document.getElementById("loginPassword")?.value;
 
       try {
-        const response = await fetch("http://localhost:3000/api/auth/login", {
+        const response = await fetch("https://deploy-amaf.onrender.com/api/login", {
           method: "POST",
           headers: {
             "Content-Type": "application/json"
--- public/scripts/signup.js
+++ public/scripts/signup.js
@@ -13,7 +13,7 @@ document.addEventListener("DOMContentLoaded", function () {
     const email = document.getElementById("email")?.value;
     const password = document.getElementById("password")?.value;
 
-    const res = await fetch("https://deploy-amaf-backend.onrender.com/api/register", {
+    const res = await fetch("https://deploy-amaf.onrender.com/api/register", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ username, email, password }),
