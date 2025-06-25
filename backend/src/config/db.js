4d432a2e5bdb007d74e82481e75ba5f1e695262e
Kijk_aq72@student.itstep.org <Kijk_aq72@student.itstep.org>
2025-06-26 00:54:15 +0300

    Добавле комитд

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,11 +1,20 @@
 const { Pool } = require('pg');
 
+// Временно выводим значение всех переменных — для отладки!
+console.log("===== PG DATABASE CONFIG =====");
+console.log("PGHOST:", process.env.PGHOST);
+console.log("PGPORT:", process.env.PGPORT);
+console.log("PGDATABASE:", process.env.PGDATABASE);
+console.log("PGUSER:", process.env.PGUSER);
+console.log("PGPASSWORD:", process.env.PGPASSWORD ? "(есть)" : "(нет)");
+console.log("================================");
+
 const pool = new Pool({
   host: process.env.PGHOST,
+  port: process.env.PGPORT,
   user: process.env.PGUSER,
   password: process.env.PGPASSWORD,
   database: process.env.PGDATABASE,
-  port: process.env.PGPORT,
   ssl: { rejectUnauthorized: false }, // для Render обязательно!
 });
 
