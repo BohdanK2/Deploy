23ce67a8f178553e6aea9dfe43533888f33ae60f
Kijk_aq72@student.itstep.org <Kijk_aq72@student.itstep.org>
2025-06-26 01:08:30 +0300

    Должно воратьд

--- public/scripts/signup.js
+++ public/scripts/signup.js
@@ -12,6 +12,12 @@ document.addEventListener("DOMContentLoaded", function () {
     const username = document.getElementById("username")?.value;
     const email = document.getElementById("email")?.value;
     const password = document.getElementById("password")?.value;
+    const password2 = document.getElementById("password2")?.value;
+
+    if (password !== password2) {
+      alert("Паролі не співпадають!");
+      return;
+    }
 
     const res = await fetch("https://deploy-amaf.onrender.com/api/register", {
       method: "POST",
